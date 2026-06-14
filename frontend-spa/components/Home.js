5545a137d6305a28625c199f61f7f8e8f158fdde
MuhammadArkham <arkhamqwe123@gmail.com>
2026-06-14 12:19:39 +0700

    style(ui): fix hero image white frame to tightly wrap the image

--- frontend-spa/components/Home.js
+++ frontend-spa/components/Home.js
@@ -321,10 +321,8 @@ const Home = Vue.defineComponent({
           </div>
 
           <!-- Right: 55% -->
-          <div class="w-full lg:w-[55%] flex justify-end" data-aos="fade-left" data-aos-duration="1200" data-aos-delay="200">
-            <div class="bg-white p-3 rounded-[2.5rem] shadow-2xl transform hover:-translate-y-2 transition-transform duration-500 max-w-[600px] w-full">
-              <img src="assets/gambar_desain.png?v=8" alt="Hero Illustration" class="w-full h-auto object-contain rounded-[2rem]">
-            </div>
+          <div class="w-full lg:w-[55%] flex justify-end items-center" data-aos="fade-left" data-aos-duration="1200" data-aos-delay="200">
+            <img src="assets/gambar_desain.png?v=9" alt="Hero Illustration" class="w-full max-w-[550px] h-auto object-cover bg-white p-2 rounded-[2rem] shadow-2xl transform hover:-translate-y-2 transition-transform duration-500">
           </div>
         </div>
       </div>
