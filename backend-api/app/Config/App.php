e5c3732ab9360d54ecb0ba061f3f957a6b11ec25
MuhammadArkham <arkhamqwe123@gmail.com>
2026-06-24 16:45:40 +0700

    fix: remove env from repo

--- backend-api/app/Config/App.php
+++ backend-api/app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'http://localhost:8080/';
+    public string $baseURL = 'http://localhost/UAS_Web2_312410545_Muhammad_Arkhamullah/backend-api/public/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
